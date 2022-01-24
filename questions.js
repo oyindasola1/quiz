3615ee8ea048aa2d3ae3656639974918094ae424
Oyin <56605776+oyindasola1@users.noreply.github.com>
2022-01-24 18:14:57 +0000

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,63 @@
+//Create an array and pass the number, questions, options and answers
+
+let questions =[
+    {
+        numb: 1,
+        question: "What do you wanna tell Joe Biden right now?",
+        answer: "Sup baby, take me out to dinner",
+        options: [
+            "Hey cutie, let's eat",
+            "Sup baby, take me out to dinner",
+            "Ayooo",
+            "Bing Bong!"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "I ain't never been with a baddie",
+        answer: "She calm so I add her to the tally",
+        options: [
+            "She calm so I add her to the tally",
+            "She calling me mummy, calling me daddy",
+            "Teenage never worry bout acne",
+            "Madison but I'm calling her maddy"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "And if you wonder if I hate you",
+        answer: "I DO!",
+        options: [
+            "Never that",
+            "I love you",
+            "I don't",
+            "I DO!"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "Stormi!!",
+        answer: "You look like mummy, baby",
+        options: [
+            "You look like mummy, baby",
+            "Where's Travis?",
+            "You're so cute",
+            "Let's work on some new kylie skin"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "You want to bam ba?",
+        answer: "You want to chill with the big boys",
+        options: [
+            "Dorime etc etc",
+            "You wanna drink with the lads",
+            "You want to chill with the big boys",
+            "You see how the thing goes"
+        ]
+    },
+];
